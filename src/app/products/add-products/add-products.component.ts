b23c21718e7e7e5c94e95bbbdef6c06de1e29f5b
manucrosspay <manu.kp@crosspaymt.com>
2023-03-24 08:58:53 +0530

    23/03/2022 edit products

--- src/app/products/add-products/add-products.component.ts
+++ src/app/products/add-products/add-products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { ProductService } from '../product.service';
 @Component({
   selector: 'app-add-products',
   templateUrl: './add-products.component.html',
@@ -9,7 +11,7 @@ export class AddProductsComponent implements OnInit{
 
   
 
-  constructor(private fb:FormBuilder)  { }
+  constructor(private fb:FormBuilder, private ps: ProductService,private router:Router)  { }
 
   ngOnInit(): void {
 
@@ -28,7 +30,30 @@ export class AddProductsComponent implements OnInit{
     review:[""],
     vendor_name:[""],
     warrenty:[""]
-  })
- 
+  }) 
+  addProduct(){
 
+    let Pdata={
+        
+      id: this.addProductForm.value.id,
+      productName: this.addProductForm.value.productName,
+      categoryId: this.addProductForm.value.categoryId,
+      description: this.addProductForm.value.description,
+      price: this.addProductForm.value.price,
+      isAvailable: this.addProductForm.value.is_available,
+      productImage: this.addProductForm.value.productImg,
+      rating: this.addProductForm.value.rating,
+      priview: this.addProductForm.value.review,
+      vendorName: this.addProductForm.value.vendor_name,
+      warrenty: this.addProductForm.value.warrenty
+    }
+
+    this.ps.addNewProduct(Pdata).subscribe((item)=>{
+      alert("new product added")
+      this.router.navigateByUrl("products")
+    })
+
+  }
+ 
+   
 }
--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -26,5 +26,16 @@ export class ProductService {
   DeleteProductComponent(productId:any){
     return this.http.delete('http://localhost:3000/products/'+productId)
   }
+
+  // api call to add new product
+  addNewProduct(productData:any){
+    return this.http.post('http://localhost:3000/products',productData)
+  }
+
+  // api call to update product
+  updateProduct(productId:any,productData:any){
+    return this.http.put('http://localhost:3000/products/'+productId,productData)
+
+  }
 }
 
